583a6a9bac07edf79772f5c681cea07c1196e8a5
sumit <sumitln2000@gmail.com>
2020-07-17 13:14:29 +0530

    Authentication added with passportjs

--- app.js
+++ app.js
@@ -2,26 +2,53 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
-mongoose.connect('mongodb://localhost:27017/gameDB',{useNewUrlParser:true, useUnifiedTopology:true});
-
-
-
-const userSchema = {
-    username:{type:String,trim:true,default:''},
-    password:{type:String,default:''},
-    bio:{type:String,default:''},
-    score:{type:Number,default:0}
-}
-
-
-const User = mongoose.model('User',userSchema);
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine","ejs");
+
+app.use(session({
+    secret:"Creative Secrets.",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect('mongodb://localhost:27017/gameDB',{useNewUrlParser:true, useUnifiedTopology:true});
 mongoose.set('useFindAndModify', false);
+
+
+const userSchema = new mongoose.Schema({
+    username:{type:String,trim:true,default:''},
+    password:{type:String,default:''},
+    bio:{type:String,default:''},
+    score:{type:Number,default:0}
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model('User',userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+});
+
+
+
 
 
 app.get("/signup",(req,res) => {
@@ -29,19 +56,16 @@ app.get("/signup",(req,res) => {
 })
 
 app.post("/signup",(req,res) => {
-    const name = req.body.username;
-    const password = req.body.password;
-    const player = new User({
-        username:name,
-        password:password
-    });
-    player.save(function(err){
+    User.register({username:req.body.username},req.body.password,function(err,user){
         if(err){
             console.log(err);
+            res.redirect("/signup");
         }else{
-            res.render("profile",{user:player});
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/profile");
+            })
         }
-    });
+    })
 });
 
 
@@ -50,77 +74,89 @@ app.get("/login",(req,res) => {
 })
 
 app.post("/login",(req,res) => {
-    const name = req.body.username;
-    const password = req.body.password;
-    User.findOne({username:name},function(err,foundUser){
-        if(!err){
-            if(foundUser.password === password){
-                res.render("play",{user:foundUser});
-            }
-        }else{
-            res.redirect("/login");
-        }
+    const user = new User({
+        username:req.body.username,
+        password:req.body.password
     });
-});
-
-
-
-
-app.get("/:userId",(req,res) => {
-    const id = req.params.userId;
-    User.findOne({_id:id},function(err,foundUser){
+    req.login(user,function(err){
         if(!err){
-            res.render("play",{user:foundUser});
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/");
+            })
         }
     })
 });
 
 
-app.post("/:userId",(req,res) => {
-    const id = req.params.userId;
+
+
+app.get("/",(req,res) => {
+    if(req.isAuthenticated()){
+        res.render("play")
+    }else{
+        res.redirect("/login");
+    }
+    
+});
+
+
+app.post("/",(req,res) => {
     const scoreData = Number(req.body.highScore);
-    User.findOneAndUpdate({_id:id},{$set:{score:scoreData}},function(err,foundUser){
-        if(err){
-            console.log(err);
+    User.findById(req.user.id,function(err,foundUser){
+        if(!err){
+            foundUser.score = scoreData;
+            foundUser.save();
         }
     });
 })
 
-
-
-app.get("/profile/:profileId",(req,res) => {
-    const requestId = req.params.profileId;
-    User.findOne({_id:requestId},function(err,foundUser){
-        if(!err){
-            res.render("profile",{user:foundUser});
-            console.log(foundUser);
-        }    
-    });
+app.get("/profile",(req,res) => {
+    if(req.isAuthenticated()){
+        User.findById(req.user.id,function(err,foundUser){
+            if(!err){
+                res.render("profile",{user:foundUser});
+            }
+        })
+    }else{
+        res.redirect("/login");
+    }
 });
 
-app.get("/profile/:profileId/edit",(req,res) => {
-    const requestId = req.params.profileId;
-    res.render("edit",{user:requestId});
+app.get("/profile/edit",(req,res) => {
+    if(req.isAuthenticated()){
+        res.render("edit");
+    }else{
+        res.redirect("/login");
+    }
 });
 
-app.post("/profile/:profileId/edit",(req,res) => {
-    const requestId = req.params.profileId;
+app.post("/profile/edit",(req,res) => {
     const editedBio = req.body.bioPara;
-    User.findOneAndUpdate({_id:requestId},{$set:{bio:editedBio}},(err,foundUser) => {
-        res.render("profile",{user:foundUser});
+    User.findById(req.user.id,function(err,foundUser){
+        if(!err){
+            foundUser.bio = editedBio;
+            foundUser.save(function(){
+                res.redirect("/");
+            });
+        }
     });
 })
 
-app.get("/rank/:userId",(req,res) => {
-    const userId = req.params.userId;
+app.get("/rank",(req,res) => {
     User.find({"score":{$ne:null}},function(err,result){
         if(!err){
-            res.render("rank",{profile:result,id:userId});
+            res.render("rank",{profile:result});
         }else{
             console.log(err);
         }
     });
 });
+
+app.get("/logout",function(req,res){
+    req.logout();
+    res.redirect("/");
+});
+
 
 
 app.listen(3000,function(){
